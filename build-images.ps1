70bc57319d7e9f1e1cc0f31dfea3faba2e261fcb
RobotCosmonaut <rondenny@gmail.com>
2025-10-03 06:27:34 -0500

    Added verification of newly built Docker images to build-images.ps1

--- build-images.ps1
+++ build-images.ps1
@@ -10,3 +10,6 @@ docker build -t musicbrainz-api-gateway:latest -f dockerfiles/Dockerfile.gateway
 docker build -t musicbrainz-streamlit-ui:latest -f dockerfiles/Dockerfile.ui .
 
 Write-Host "All images built successfully!" -ForegroundColor Green
+
+# Verification list of all the MusicBrainz images that were just built by the script
+docker images | Select-String -Pattern "musicbrainz"
